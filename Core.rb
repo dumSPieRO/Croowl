3280a223639e8355932c3ad8569bdd2bd5214553
adamovich <Sergey211998i@gmail.com>
2019-08-23 15:14:10 +0300

    Tests

--- Core.rb
+++ Core.rb
@@ -1,6 +1,80 @@
-require curb
-require nokogiri
+require 'curb'
+require 'nokogiri'
 
-def class loader
+class Loader
+  def load(urls)
 
+  end
 end
+
+class Test
+
+  def start_testing(*params)
+    puts "Start testing"
+  end
+
+  def finish_testing(*params)
+    puts "Finish test"
+  end
+
+  def result?(result, test_result )
+    test_result == result
+  end
+
+  def get_test_results(*params)
+    [1, 2, nil, 3, 4, 5, nil]
+  end
+
+  def get_method_result(method, *params)
+    #Todo: Block rename
+    block = lambda{
+      puts "  Params #{params}"
+      method.call(*params)
+    }
+    params == [] ? method.call :  block.call
+  end
+
+  def get_params(*params)
+    [1, 2, 3, 5, 5]
+  end
+
+  def run(method)
+    correct = 0
+    incorrect = 0
+    incorrectly = []
+    errors = 0
+    bl = lambda { |elem|
+      incorrect += 1
+      incorrectly.append(elem)
+    }
+    start_testing
+    puts "Method: #{method}"
+    tests  = get_test_results
+    params = get_params
+    tests.each_with_index do |current_test, i|
+      puts " #{i}. Start test number #{i}"
+      start_testing
+      puts " Method #{method.name} say:"
+      res = get_method_result(method, *params[i])
+      result = result?(res, current_test)
+      result ? correct += 1 : bl.call(i)
+      puts " Result test numb #{i}:: \n  :(T-> \"#{current_test}\" ---> R-> \"#{res}\"):#{result} "
+      finish_testing
+      puts "Finish test number #{i}\n "
+    end
+    puts "Method: #{method}"
+    puts "Result tests::"
+    puts "            ::Sum: #{tests.length}"
+    puts "            ::Correct: #{correct}"
+    puts "            ::Incorrectly: #{incorrect} --> (#{incorrectly})"
+    puts "            ::Errors: #{errors}"
+  end
+end
+
+def lol(*pa)
+end
+
+test = Test.new
+meth = method(:lol)
+
+puts test.run(meth)
--- Data.rb
+++ Data.rb
@@ -1,1 +1,63 @@
+class EducationGarfield < Scripting::CustomParser
 
+  def solo_product(doc)
+    opt = doc.xpath("//div[@class='product-item-detail-info-section']")
+    name = opt.xpath('./@title').text
+    p[NAME] = name + i.to_s
+    price = doc.xpath('//meta[@itemprop="price"]/@content')
+    if price.first
+      p[PRICE] = price(price.first.value)
+    end
+    old_price = opt.xpath(".//@data-old-price")
+    if old_price.first
+      p[REGULAR_PRICE] = price(old_price.first.value)
+    end
+    stock = opt.xpath("//span[@class='available']")
+    if stock.first
+      p[STOCK] = stock.first.text == "В наличии" ? "In stock" : "Out of stock"
+    end
+    promo_name = opt.xpath(".//@data-discountstatus")
+    if promo_name.first
+      p[PROMO_NAME] = price.first.value != "" ? "Акция" : nil
+    end
+    sku = opt.xpath('./@data-art').text
+    p[SKU] = sku != "" ? sku : nil
+    p[KEY] = name + "1"
+    return p
+  end
+
+  def multi_product(multi, prices)
+    i = 0
+    multi.each do |opt|
+      name = opt.xpath('./@title').text
+      p[NAME] += ", " + name
+      p[PRICE] = price(prices[i].value)
+      sku = opt.xpath('./@data-art').text
+      p[SKU] = sku != "" ? sku : nil
+      p[KEY] = name + i.to_s
+      p[REGULAR_PRICE] = price(opt.xpath(".//@data-old-price").first.value)
+      p[STOCK] = opt.xpath(".//@data-availstatus").first.value == "not_avail" ? "Out of stock" : "In stock"
+      p[PROMO_NAME] = opt.xpath(".//@data-discountstatus").first.value != "" ? "Акция" : ""
+      i += 1
+    end
+    return p
+  end
+
+  def parse(ctx)
+    base = ctx.base_product
+    doc = ctx.doc
+    if base
+      p = base.dup
+      prices = doc.xpath('//meta[@itemprop="price"]/@content')
+      multi = doc.xpath("//div[@class='product-item-detail-info-section']//ul[contains(@class, 'product-item-scu-item-list item_prop_list')]//li")
+      if multi.first
+        p = multi_product(multi, prices)
+        ctx.add_product(p)
+      end
+    else
+      p = solo_product(doc)
+      ctx.add_product(p)
+    end
+    !ctx.products.empty?
+  end
+end
